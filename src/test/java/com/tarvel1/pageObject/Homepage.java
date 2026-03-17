a01fb761f67088e73900fd58750a86716f91b0f6
rajatprasad7 <119569289+rajatprasad7@users.noreply.github.com>
2026-03-17 22:11:57 +0530

    Modify comment in Homepage.java
    Updated comment for clarity in Homepage.java.

--- src/test/java/com/tarvel1/pageObject/Homepage.java
+++ src/test/java/com/tarvel1/pageObject/Homepage.java
@@ -16,7 +16,7 @@ public class Homepage {
 	@FindBy(xpath="//*[@id=\"menu-item-43\"]/a") WebElement Home;
 
 //@FindBy(xpath="//a[normalize-space()=Home]") WebElement home;
-	//Action
+	//Action hello
 	
 @FindBy(xpath="//input[@id='form_first_name_7']")  WebElement name;
 @FindBy(xpath="//input[@id='form_email_7']") WebElement emailAddress;
